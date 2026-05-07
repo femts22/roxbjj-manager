1f498d3d998fa2bec83e6a43300abd74b9eac121
Felipe <felipematheus@venddor.com.br>
2026-05-07 20:50:15 -0300

    Removendo arquivos do Capacitor para o Deploy

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.roxbjj.manager',
-  appName: 'ROXBJJ Manager',
-  webDir: 'out'
-};
-
-export default config;
